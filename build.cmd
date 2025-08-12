453c7823d5272987980f21ad7333c672b3855439
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-08-12 18:37:23 -0300

    new build script

--- build.cmd
+++ build.cmd
@@ -1,4 +1,12 @@
 @echo off
-pip install pyinstaller
-pyinstaller --onefile --noconsole --strip --clean --name ntcleaner src\main.py
+pip install --quiet pyinstaller
+
+pyinstaller --onefile --strip --clean --name ntcleaner src\main.py
+
+echo.
+echo ===== Build finished. =====
+echo.
+
 dist\ntcleaner.exe -h
+
+pause
